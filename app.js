9c0cd11a1ddfba729e6ee9160b718b8bfc53945b
Jordan Reynolds <jreynolds@hublsoft.com>
2021-03-31 19:40:44 +0100

    restructure files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/route");
+const { handleErrors } = require("./errors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.use("/games", router);
+
+app.use(handleErrors);
+
+
+module.exports = app;
