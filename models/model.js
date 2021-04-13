221e32b542147a1c1723524dc0afeb162b6a296a
Jordan Reynolds <jreynolds@hublsoft.com>
2021-04-13 15:33:02 +0100

    poll for game state

--- models/model.js
+++ models/model.js
@@ -19,7 +19,7 @@ exports.updatePlayerTwo = (name, gameId) => {
             return Promise.reject({ status: 409, msg: "Another player has already joined this game" })
         }
 
-        parsedGames[gameIndex] = {...parsedGames[gameIndex], player2: name, state: "ready", board: []};
+        parsedGames[gameIndex] = {...parsedGames[gameIndex], player2: name, state: "ready", board: new Array(9)};
         return fs.writeFile(fileName, JSON.stringify(parsedGames), "utf8")
     })
 }
@@ -28,7 +28,7 @@ exports.selectGame = (gameId) => {
     return fs.readFile(fileName, "utf8").then((games) => {
         const game = JSON.parse(games).find(g => g.gameId === gameId);
         if (!game) {
-            return Promise.reject({ status: 404, msg: "invalid gameId" });
+            return Promise.reject({ status: 404, msg: "Invalid gameId" });
         } else {
             return game;
         }
@@ -39,8 +39,16 @@ exports.updatePlayerMove = (gameId, player, squareIndex) => {
     return fs.readFile(fileName, "utf8").then((games) => {
         const parsedGames = JSON.parse(games);
         const gameIndex = parsedGames.findIndex(g => g.gameId === gameId);
-        const playerMove = player === "player1" ? "X" : "0";
-        parsedGames[gameIndex].board[squareIndex] = playerMove;
-        return fs.writeFile(fileName, JSON.stringify(parsedGames), "utf8")
+        const currentBoard = parsedGames[gameIndex].board;
+
+        if (currentBoard[squareIndex]) {
+            return Promise.reject({ status: 404, msg: "Square already taken" });
+        } else if (squareIndex > currentBoard.length - 1) {
+            return Promise.reject({ status: 404, msg: "Invalid square" });
+        } else {
+            parsedGames[gameIndex].board[squareIndex] = player === "player1" ? "X" : "0";
+            return fs.writeFile(fileName, JSON.stringify(parsedGames), "utf8");
+        }
+
     })
 }
